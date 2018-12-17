4340961d7adeb28e01ad67a5cfc70aa58c586d56
Nicholas Grasevski <grasevski@gmail.com>
2018-12-17 20:13:35 +1100

    Added code

---
+++ stan.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", stan)
+	check(http.ListenAndServe(":8080", nil))
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+type entry struct {
+	Image string `json:"image"`
+	Slug  string `json:"slug"`
+	Title string `json:"title"`
+}
+
+func stan(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	var (
+		req struct {
+			Payload []struct {
+				Drm          bool `json:"drm"`
+				EpisodeCount int  `json:"episodeCount"`
+				Image        struct {
+					ShowImage string `json:"showImage"`
+				} `json:"image"`
+				Slug  string `json:"slug"`
+				Title string `json:"title"`
+			} `json:"payload"`
+		}
+		encoder = json.NewEncoder(w)
+	)
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		check(encoder.Encode(struct {
+			Error string `json:"error"`
+		}{"Could not decode request: JSON parsing failed"}))
+		return
+	}
+	var res struct {
+		Response []entry `json:"response"`
+	}
+	for _, x := range req.Payload {
+		if x.Drm && x.EpisodeCount > 0 {
+			res.Response = append(res.Response, entry{x.Image.ShowImage, x.Slug, x.Title})
+		}
+	}
+	check(encoder.Encode(res))
+}
